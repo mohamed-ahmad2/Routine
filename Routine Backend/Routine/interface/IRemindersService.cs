f3e3cb5774d9f8fcd9ec571d8aa5cf6da0183f3e
loki <Abdohamdy4587gmail.com>
2026-06-22 11:15:01 +0300

    get

---
+++ Routine Backend/Routine/interface/IRemindersService.cs
@@ -0,0 +1,9 @@
+﻿using Routine.DTO;
+
+namespace Routine.Interface
+{
+    interface IRemindersService
+    {
+        Task<List<ReminderDto>> GetByUserIdAsync(int userId, CancellationToken cancellationToken = default);
+    }
+}
